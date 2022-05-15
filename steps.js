72da6a50342943b5c82e4d0ea92165855336325d
Mohammad Imran <jobaierimran@gmail.com>
2022-05-15 18:14:59 +0600

    Update steps.js

--- steps.js
+++ steps.js
@@ -1,1 +1,18 @@
 
+const express = require('express');
+const cors = require('cors')
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+//use middleware
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Running my node server');
+});
+
+app.listen(port, () => {
+  console.log('Listening to port', port);
+});
